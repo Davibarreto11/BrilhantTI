2cce8d87c0a9fb8e4187f3f4a92a4b2c02838f2a
Davi Barreto <daviarturdesa686@gmail.com>
2024-03-20 00:09:59 -0300

    Configuration project

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+const config = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "cor-bg": "#81b398",
+      },
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+      "3xl": "1920px",
+      "4xl": "2561px",
+    },
+  },
+  plugins: [],
+};
+
+export default config;
