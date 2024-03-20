18ba1908cd6ce36afcf13487add21c91e5b67dc6
Davi Barreto <daviarturdesa686@gmail.com>
2024-03-20 20:09:36 -0300

    Section Component

---
+++ src/components/Section/index.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+interface SectionProps {
+  children: any;
+  level: "about" | "technologies" | "services" | "contacts";
+}
+
+const Section: React.FC<SectionProps> = ({ children, level }) => {
+  return (
+    <div
+      id={level}
+      className={`flex justify-center ${
+        level === ("technologies" || "contacts") ? "bg-white" : ""
+      }`}
+      style={{ minHeight: "100vh" }}
+    >
+      <div className="flex" style={{ width: "80%" }}>
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default Section;
