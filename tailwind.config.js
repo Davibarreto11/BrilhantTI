cee3bce09874a340ba5adf23c4a392624f305caa
Davi Barreto <daviarturdesa686@gmail.com>
2024-03-20 20:08:59 -0300

    COLORS in tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,14 @@ const config = {
   theme: {
     extend: {
       colors: {
-        "cor-bg": "#81b398",
+        "primaryGray": "#2B2D42",
+        "primaryPurple": "#2E0249",
+        "pimaryPink": "#702B9E",
+        "primaryBlue": "#5591F5",
+        "secondaryGray": "#2d2b42",
+        "secondaryPurple": "#4E2C70",
+        "secondaryPink": "#9C43F8",
+        "tertiaryPurple": "#855DF7",
       },
     },
     screens: {
@@ -12,9 +19,9 @@ const config = {
       md: "768px",
       lg: "1024px",
       xl: "1280px",
-      "2xl": "1536px",
+      "2xl": "1439px",
       "3xl": "1920px",
-      "4xl": "2561px",
+      "4xl": "2560px",
     },
   },
   plugins: [],
