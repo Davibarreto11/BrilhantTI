f1545d38963bb708050aca9501cf2c33d7aa9def
Davi Barreto <daviarturdesa686@gmail.com>
2024-03-20 01:04:24 -0300

    COLORS project

---
+++ src/styles/theme.ts
@@ -0,0 +1,19 @@
+interface Color {
+  primaryGray: string;
+  primaryPurple: string;
+  pimaryPink: string;
+  primaryBlue: string;
+  secondaryPurple: string;
+  secondaryPink: string;
+  tertiaryPurple: string;
+}
+
+export const COLORS: Color = {
+  primaryGray: "#2B2D42",
+  primaryPurple: "#2E0249",
+  pimaryPink: "#702B9E",
+  primaryBlue: "#5591F5",
+  secondaryPurple: "#4E2C70",
+  secondaryPink: "#9C43F8",
+  tertiaryPurple: "#855DF7",
+};
